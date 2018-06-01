b725010fff6253114a28ad8b5a24e94d3d94a87d
Mohammad Nizam Uddin <sha0143@hotmail.com>
2018-06-01 18:53:44 +0200

    obf add user module added in user management

--- imports/service/onlyAuthorisedUserGuard.ts
+++ imports/service/onlyAuthorisedUserGuard.ts
@@ -2,7 +2,7 @@ import { Injectable, NgZone, ApplicationRef } from '@angular/core';
 import { Accounts } from 'meteor/accounts-base';
 import { Router, ActivatedRoute, Params, NavigationEnd, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Meteor } from 'meteor/meteor';
-// import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 import { AccountService } from '/imports/service/accountService';
 
@@ -16,19 +16,64 @@ export class OnlyAuthorisedUserGuard implements CanActivate {
   }
 
 
+  // canActivate(route: ActivatedRouteSnapshot,
+  //             state: RouterStateSnapshot) : Observable<boolean> | Promise<boolean> | boolean {
+  //
+  //
+  //                if (this.accountService.isAuthenticated()) {
+  //                   console.log('authenticated');
+  //                    return true;
+  //                  } else {
+  //                    window.alert("You don't have permission to view this page");
+  //                    this.router.navigate(['/']);
+  //                    return false;
+  //                  }
+  //
+  //
+  // } // END OF canActivate
+
+
+
   canActivate(route: ActivatedRouteSnapshot,
-              state: RouterStateSnapshot) : Promise<boolean> | boolean {
+              state: RouterStateSnapshot) : Observable<boolean> | Promise<boolean> | boolean {
+
+               if (this.isPageRefresh()) {
 
 
-                 if (this.accountService.isAuthenticated()) {
+                 return this.accountService.isAuthenticated()
+                    .then(
+                      (authenticated : boolean) => {
+                        if (authenticated) {
+                          return true;
+                        } else {
+                          window.alert("You don't have permission to view this page");
+                          this.router.navigate(['/']);
+                          return false;
+                        } // END OF if else authenticated
+                      } // END OF (authenticated : boolean)
+                    ); // END OF .then
+
+
+               } else {
+                 if (this.accountService.isLoggedIn()) {
                      return true;
                    } else {
                      window.alert("You don't have permission to view this page");
                      this.router.navigate(['/']);
                      return false;
                    }
-
+               }
 
   } // END OF canActivate
+
+  // I determine if the current route-request is part of a page refresh.
+  isPageRefresh() : boolean {
+
+      // If the router has yet to establish a single navigation, it means that this
+      // navigation is the first attempt to reconcile the application state with the
+      // URL state. Meaning, this is a page refresh.
+      return( ! this.router.navigated );
+
+  }
 
 } // END OF OnlyAuthorisedUsersGuard
